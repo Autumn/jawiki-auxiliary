dad57b8126510efe7f35a989dea59c28c361a75d
Andrew Coleman <andrewpcoleman@gmail.com>
2013-04-18 11:32:23 +1000

    initial commit

---
+++ mw_text_extract.rb
@@ -0,0 +1,149 @@
+require 'nokogiri'
+
+class Parser
+  def initialize(text)
+    @pos = 0
+    @text = text
+  end
+
+  def nextChar(*n)
+    if n.length == 0
+      @pos += 1
+    else
+      @pos += n[0]
+    end
+  end 
+
+  def printChar
+    print @text[@pos]
+  end
+
+  def parseLink
+    print "[\"link\", \""
+    while checkDelimiter("]", "]") == false
+      if checkDelimiter("[", "[")
+        nextChar
+        print "\", "
+        parseLink
+        print ", \""
+      end
+      print @text[@pos]
+      nextChar
+    end
+    print "\"]"
+    nextChar(2)
+  end
+
+  def parseTemplate
+#    print "["
+    while varDelimiter("}", "}") == false
+      if varDelimiter("{", "{") #checkDelimiter("{", "{")
+        nextChar(2)
+        parseTemplate
+      end
+#      print @text[@pos]
+      nextChar
+    end
+#    print "]"
+    nextChar(2)
+  end
+
+  def checkDelimiter(first, second)
+    if @text[@pos] == first and @text[@pos+1] == second
+      return true
+    end
+    false
+  end
+
+  def startTemplate
+    varDelimiter("{", "{")
+  end
+
+  def startLink
+    varDelimiter("[", "[")
+  end
+
+  def varDelimiter(*delims)
+    delims.length.times do |i|
+      if delims[i] != @text[@pos+i]
+        return false
+      end
+    end
+    true
+  end
+
+  def checkFormatting
+    if varDelimiter("\'", "\'", "\'", "\'", "\'")
+      return "bold+italic"
+    elsif varDelimiter("\'", "\'", "\'")
+      return "bold"
+    elsif varDelimiter("\'", "\'")
+      return "italic"
+    end
+    return ""
+  end
+
+ def skipFormatting 
+   if varDelimiter("\'", "\'", "\'", "\'", "\'")
+     nextChar(5)
+   elsif varDelimiter("\'", "\'", "\'")
+     nextChar(5)
+   elsif varDelimiter("\'", "\'")
+     nextChar(5)
+   end
+ end
+
+  def parse
+    while @pos < @text.length
+      if startLink
+        nextChar(2)
+        parseLink
+      elsif startTemplate
+        nextChar(2)
+        parseTemplate
+      elsif varDelimiter("{", "|") 
+        nextChar(2)
+        while not varDelimiter("|", "}")
+          nextChar
+        end
+        nextChar(2)
+      elsif @text[@pos] == "\n"
+        printChar
+        nextChar(1)
+        # check heading
+        if varDelimiter("=", "=")
+          nextChar(2)
+          print "[\"h2\", \""
+          while not varDelimiter("=", "=")
+            print @text[@pos]
+            nextChar
+          end
+          nextChar(2)
+          print "\"]"
+        # check bullet lists
+        elsif varDelimiter("*")
+
+        end
+      else
+        ### change - formatting can occur /anywhere/ 
+        if @text[@pos] == "\'"
+          if checkFormatting != ""
+             skipFormatting 
+          end
+        end
+        print @text[@pos]
+        nextChar
+      end
+    end
+  end
+
+end
+
+file = open("2")
+article = Nokogiri::HTML file.read
+article.encoding = 'utf-8'
+text = article.xpath("//text").text
+
+parser = Parser.new(text)
+
+parser.parse
