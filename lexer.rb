8652d7547c9d6d1f43fb8b0338ec592c12fbe9e3
Andrew Coleman <andrewpcoleman@gmail.com>
2013-04-22 21:49:02 +1000

    Cleaned up some code, added redirect and hr rules. Scaffolded for anywhere token parsing. Stub functions? More like procrastination functions.

--- lexer.rb
+++ lexer.rb
@@ -49,42 +49,57 @@ class Lexer
     str
   end 
 
-  def get_start_of_line_token
-    if @doc[@pos] == "\n"
+  def anywhere_token(text)
+    tokens = Array.new
+    text
+  end
+
+  def next_line_token
+    if @pos == @doc.length
+      "EOF"
+    elsif scanChar(0) == "\n"
       nextChar
-      [:p]
-    elsif @doc[@pos] == "{" 
-      if scanChar == "|"
-        nextChar 2
-        [:table, read_until("|", "}")]
-      elsif scanChar == "{"
-        nextChar 2
-        [:category, read_until("}", "}")]
+      [:p, "\n"]
+    elsif delimiter_reached? ["{", "|"]
+      nextChar 2
+      [:table, read_until("|", "}")]
+    elsif delimiter_reached? ["{", "{"]
+      nextChar 2
+      text = read_until("}", "}")
+      # parse text with anywhere rules
+      [:category, text]
+    elsif delimiter_reached?(["=", "="])
+      # determine heading level
+
+      h = 0
+
+      until scanChar(h) != "="
+        h += 1
       end
-    elsif @doc[@pos] == "="
-      if scanChar == "=" # heading 
-        # determine heading level
 
-        h = 0
-
-        until scanChar(h) != "="
-          h += 1
-        end
-
-        nextChar h
-        
-        [:heading, read_until(*("=" * h).split(""))]
-      end
+      nextChar h
+      text = read_until(*("=" * h).split("")) # * is the splat operator
+      # parse text with anywhere rules
+      [:heading, text]
+    elsif scanChar(0) == "#" and scanChar(1).downcase == "r"
+      [:redirect, get_to_end_of_line]
     elsif ["*", "#", ":", ";"].include? scanChar(0)
       # list
       list = Array.new
       while ["*", "#", ":", ";"].include? scanChar(0)
-        list.push get_to_end_of_line
+        text = get_to_end_of_line
+        # parse text with anywhere rules
+        list.push text
         nextChar
       end
       [:list, list]
+    elsif delimiter_reached?(["-", "-", "-", "-"])
+      [:hr, get_to_end_of_line] 
     else
-      [:text, get_to_end_of_line]
+      text = get_to_end_of_line
+      # parse text with anywhere rules
+      [:text, anywhere_token(text)]
+      #[:text, text]
     end
   end
 
@@ -93,43 +108,21 @@ class Lexer
     while @doc[@pos] != "\n" and @pos != @doc.length
       nextChar
     end
-    [@doc[start..@pos]]
+    [@doc[start..@pos-1]] # @pos - 1 to ignore the newline
   end
-
-  def start_of_line
-    # blank - new paragraph
-    # list - care required
-    # table - scan to end of table and return entire blob
-    # text - read until end of line, then parse token separately for links/formatting/other things
-    # horizontal rule - remove
-    # redirect - use
-    start = @pos
-  
-    while @doc[@pos] != "\n" and @pos != @doc.length
-      nextChar
-    end
-    [@doc[start..@pos]]
-  end
-
-  def next_token
-    if @pos == 0
-      get_start_of_line_token
-    elsif @doc[@pos] == "\n"
-      nextChar
-      get_start_of_line_token
-    elsif @pos == @doc.length 
-      "EOF"
-    else
-      [:text, get_to_end_of_line]
-    end
-  end 
 
   def scan
+    first_scan_tokens = Array.new
     tokens = Array.new
-    while (token = next_token) != "EOF"
-      tokens.push token
+    while (token = next_line_token) != "EOF"
+      first_scan_tokens.push token
     end
-    puts "#{tokens}"
+
+puts "#{first_scan_tokens}"
+    first_scan_tokens.each do |token|
+      # scan every token for anywhere symbols
+      
+    end
   end
 
 end
